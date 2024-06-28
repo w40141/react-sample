89ba6750885665f5b98b4d469aee7df73475297a
w40141 <w.40141@gmail.com>
2024-06-28 18:46:24 +0900

    feat: 盤面は終わった

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function Square() {
+	const [value, setValue] = useState(null);
+	function handleClick() {
+		setValue("X");
+	}
+	return (
+		<button type="button" className="square" onClick={handleClick}>
+			{value}
+		</button>
+	);
+}
+
+export default function Board() {
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	return (
+		<>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className="board-row">
+				<Square />
+				<Square />
+				<Square />
+			</div>
+		</>
+	);
+}
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-// import "./App.css";
-
-export default function App() {
-	return (
-		<button type="button" className="square">
-			X
-		</button>
-	);
-}
